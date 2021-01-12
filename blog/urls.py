b6ce7fbebe448476fb077c78d3a650e422bb1978
Dragos Homner <dragos.homner@gmail.com>
2021-01-12 20:59:02 +0200

    add comment form

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.article_index, name="article_index"),
     path("<int:pk>/", views.article_detail, name="article_detail"),
+    path("categories/", views.category_index, name="category_index"),
+    path("category/<int:category_pk>/", views.article_by_category_index, name="article_by_category_index"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from blog.models import Article
+from blog.models import Article, Category, Comment
+from blog.forms.CommentForm import CommentForm
 
 # Create your views here.
 
@@ -12,7 +13,38 @@ def article_index(request):
 
 def article_detail(request, pk):
     article = Article.objects.get(pk=pk)
+    comments = Comment.objects.filter(article=article)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                article=article
+            )
+            comment.save()
+
     context = {
-        'article': article
+        'article': article,
+        'comments': comments,
+        'form': form
     }
     return render(request, 'article_detail.html', context)
+
+def category_index(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'category_index.html', context)
+    
+def article_by_category_index(request, category_pk):
+    category = Category.objects.get(pk=category_pk)
+    articles = Article.objects.filter(category=category)
+    context = {
+        'articles': articles,
+        'category_name': category.name
+    }
+    return render(request, 'article_index.html', context)
